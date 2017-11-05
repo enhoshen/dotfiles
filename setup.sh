dca087a91d91e438f2dd506781cd179bbc66ed0f
enhoshen <enhoshen@gmail.com>
2017-11-05 11:36:10 +0800

    trivial

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
 dir=${PWD}
 echo ${dir}
 echo "ln -sf ${dir}/.vimrc ~/.vimrc" >> ~/.bashrc
-echo "ln -sf ${dir}/.bash_aliases ~/bash_aliases" >> ~/.bashrc
-source ~/.bashrc
+echo "ln -sf ${dir}/.bash_aliases ~/.bash_aliases" >> ~/.bashrc
+echo ". ~/.bash_aliases" >> ~/.bashrc
+. ~/.bashrc
