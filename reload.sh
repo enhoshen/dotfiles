3a541cc4137c584e5dfb9120a7eea5dc2a07de83
enhoshen <enhoshen@media.ee.ntu.edu.tw>
2020-03-02 10:43:34 +0800

    Reload now has c and f arguments that takes command index and alternative command file respectively; Add some color highlight

--- reload.sh
+++ reload.sh
@@ -1,36 +1,59 @@
 #!/bin/bash
-echo -e "Usage: $ sh reload.sh [-e] [file] \n -e: print out the command; default history file:.myhistory;"
-echo -e "To add new command to a file, just bring up the command use $ echo \"command\" >> {file} \n"  
+color="\033[36m"
+colorrst="\033[0m"
+echo -e "Usage:" $color "$ sh reload.sh [-e] [file]${colorrst}\n" \
+    "${color}-e${colorrst}: print out the command;\n"\
+    "${color}-f${colorrst}: the file path of the command list. default history file:./.myhistory;\n"\
+    "${color}-c${colorrst}: the command number. If not provided the script prompt you to enter one.\n"
+echo -e "To add new command to a file, just bring up the command and use ${color}$ echo \"command\" >> {file} ${colorrst}\n"  
 # -e argument enable interpretation of backslash escapes for echo
 _echo=0
 file=.myhistory
-for i in "$@"
+IN=''
+#for i in "$@"
+#do
+#    case $i in
+#        -e)
+#        _echo=1
+#        ;;
+#        *)
+#            if [ $i!='' ];
+#            then
+#                file=$i      
+#            fi
+#        ;;
+#    esac
+#done
+while getopts 'efc:' opt
 do
-    case $i in
-        -e)
-        _echo=1
-        ;;
-        *)
-            if [ $i!='' ];
-            then
-                file=$i      
-            fi
-        ;;
+    case "$opt" in
+        e ) _echo=1 ;;
+        f ) file="$OPTARG" ;;
+        c ) IN="$OPTARG" ;;
+        #? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+        ?) : ;;
     esac
 done
 
+echo $_echo $file $IN
 readarray commands < $file
 for i in "${!commands[@]}"
 do
-    echo ${i} ${commands[i]}
+    echo -e $color ${i} $colorrst ${commands[i]}
 done
 
-echo -n "choose commands "
-read IN
+if ! [[ ${IN} =~ ^[0-9]+$ ]];
+then
+    echo -n "choose command "
+    read IN
+else
+    : #null command (like pass in python)
+fi
 
 if [[ $_echo == 1 ]];
 then
-    echo ${commands[IN]} | xclip
+    echo -e $color $IN $colorrst ${commands[IN]}
+    echo -e $color ${commands[IN]} $colorrst | xclip
 else
     eval ${commands[IN]}
 fi
