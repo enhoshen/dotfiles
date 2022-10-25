2d1f4505dada21e19d305f1a677c089ec414aad1
enhoshen <enhoshen@gmail.com>
2022-10-25 14:24:02 +0800

    Add fzf and nvim installation commands in setup.sh

--- setup.sh
+++ setup.sh
@@ -23,3 +23,14 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall
 vim +PlugUpdate +qall
+
+#nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+
+# fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# python
+python -m pip install neovim
+python -m pip install tmuxp 
