ac446f871aaf5db1146a47d4fff2d5ddb921a601
enho.shen <enho.shen@ganzin.com.tw>
2025-04-07 23:59:51 +0800

    feat: Customize my own dapui layouts

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -14,7 +14,68 @@ return {
       local dap = require("dap")
       local ui = require("dapui")
 
-      require("dapui").setup()
+      opts = {
+        icons = { expanded = "", collapsed = "", current_frame = "" },
+        mappings = {
+          -- Use a table to apply multiple mappings
+          expand = { "<CR>", "<2-LeftMouse>" },
+          open = "o",
+          remove = "d",
+          edit = "e",
+          repl = "r",
+          toggle = "t",
+        },
+        element_mappings = {},
+        expand_lines = vim.fn.has("nvim-0.7") == 1,
+        force_buffers = true,
+        layouts = {
+          {
+            -- You can change the order of elements in the sidebar
+            elements = {
+              -- Provide IDs as strings or tables with "id" and "size" keys
+              --{ id = "breakpoints", size = 0.25 },
+              -- Can be float or integer > 1
+              --{ id = "scopes", size = 0.25 },
+              --{ id = "stacks", size = 0.25 },
+              --{ id = "watches", size = 0.25 },
+              { id = "scopes", size = 0.5 },
+              { id = "repl", size = 0.5 },
+            },
+            size = 80,
+            position = "right", -- Can be "left" or "right"
+          },
+          --- { elements = { "repl", "console", }, size = 10, position = "bottom", -- Can be "bottom" or "top" },
+        },
+        floating = {
+          max_height = nil,
+          max_width = nil,
+          border = "single",
+          mappings = {
+            ["close"] = { "q", "<Esc>" },
+          },
+        },
+        controls = {
+          enabled = vim.fn.exists("+winbar") == 1,
+          element = "repl",
+          icons = {
+            pause = "",
+            play = "",
+            step_into = "",
+            step_over = "",
+            step_out = "",
+            step_back = "",
+            run_last = "",
+            terminate = "",
+            disconnect = "",
+          },
+        },
+        render = {
+          max_type_length = nil, -- Can be integer or nil.
+          max_value_lines = 100, -- Can be integer or nil.
+          indent = 1,
+        },
+      }
+      ui.setup(opts)
 
       require("nvim-dap-virtual-text").setup({
         -- This just tries to mitigate the chance that I leak tokens here. Probably won't stop it from happening...
