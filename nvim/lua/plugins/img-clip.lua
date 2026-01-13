f36b6869a0ca7ea5590293cb03af22e1852c2655
enhoshen <enhoshen@gmail.com>
2026-01-14 02:25:03 +0800

    nvim: Setup img-clip image save folder

---
+++ nvim/lua/plugins/img-clip.lua
@@ -0,0 +1,19 @@
+return {
+  "HakonHarnes/img-clip.nvim",
+  event = "VeryLazy",
+  opts = {
+    default = {
+      dir_path = "images",
+      use_absolute_path = false,
+      relative_to_current_file = true,
+    },
+  },
+  keys = {
+    -- suggested keymap
+    {
+      "<leader>p",
+      "<cmd>PasteImage<cr>",
+      desc = "Paste image from system clipboard",
+    },
+  },
+}
