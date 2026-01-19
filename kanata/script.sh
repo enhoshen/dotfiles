03f2bd36c656ec28e764efcb12877519442623d7
enhoshen <enhoshen@gmail.com>
2026-01-20 03:28:52 +0800

    kanata: Add macro layer; Add tf2 server command macro

--- kanata/script.sh
+++ kanata/script.sh
@@ -1,8 +1,8 @@
 INSTALL_PATH=/mnt/g/portableprogram/kanata/
 FILES=(config.json menus.json)
 wsl_pull() {
-  cp ${INSTALL_PATH}/kanata.kbd kanata/
+  cp ${INSTALL_PATH}/*.kbd kanata/
 }
 wsl_install() {
-  cp kanata/kanata.kbd ${INSTALL_PATH}/
+  cp kanata/*.kbd ${INSTALL_PATH}/
 }
