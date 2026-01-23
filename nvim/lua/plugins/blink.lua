7f138f2b877013913c863dc01aa0c1c1bbdc5e5b
enhoshen <enhoshen@gmail.com>
2026-01-24 01:11:30 +0800

    kanata,nvim: Add noclip, kill macro for tf2 in kanata; Display source for blink.cmp items

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,6 +1,24 @@
 return {
   "saghen/blink.cmp",
   opts = {
+    completion = {
+      menu = {
+        draw = {
+          source_name = {
+            width = { max = 30 },
+            text = function(ctx)
+              return ctx.source_name
+            end,
+            highlight = "BlinkCmpSource",
+          },
+          columns = {
+            { "label", "label_description", gap = 1 },
+            { "kind_icon", "kind" },
+            { "source_name" },
+          },
+        },
+      },
+    },
     sources = {
       providers = {
         snippets = {
