3d5dc3ea3d3155397da8a85b613e2c721fc870f5
enhoshen <enhoshen@gmail.com>
2026-02-12 02:18:45 +0800

    nvim: Tweak mark A and quickfix toggle keymap

--- nvim/lua/config/keymaps/make.lua
+++ nvim/lua/config/keymaps/make.lua
@@ -7,16 +7,18 @@ vim.keymap.set("n", "<Leader>ms", function()
 end, { desc = "Pytest: Make with -s and log level debug" })
 
 -- Keymap to toggle right pane/buffer
+local mark_A_open = false
 vim.keymap.set("n", "<Leader>ml", function()
-  -- If there's more than one window, assume the right pane is open and close it.
-  -- Otherwise, open the right pane with a new vertical split.
-  if vim.fn.winnr("$") > 1 then
+  -- * don't know how to check if mark A window is already closed
+  -- * don't know how to check if focus is still on the mark A window 
+  if mark_A_open then
     vim.cmd("close") -- Closes the current window. Assumes focus is on the pane to be closed.
   else
     -- Opens a new vertical split, moves to it, and jumps to mark A.
     vim.cmd([[vnew | norm <C-w>L ]])
     vim.cmd([[norm 'A]])
   end
+  mark_A_open = not mark_A_open
 end, {
   desc = "Toggle mark A at right Pane, assuming mark A points to tmp/make.txt",
   noremap = true,
--- nvim/lua/config/keymaps/quickfix.lua
+++ nvim/lua/config/keymaps/quickfix.lua
@@ -1,13 +1,13 @@
 -- Keymap to toggle right pane/buffer
+local qlist_open = false
 vim.keymap.set("n", "<Leader>qo", function()
-  -- If there's more than one window, assume the right pane is open and close it.
-  -- Otherwise, open the right pane with a new vertical split.
-  if vim.fn.winnr("$") > 1 then
-    vim.cmd("close") -- Closes the current window. Assumes focus is on the pane to be closed.
+  if qlist_open then
+    vim.cmd([[cclose]])
   else
     -- Open quickfix and swap to it
     vim.cmd([[cope | wincmd L]])
   end
+  qlist_open = not qlist_open
 end, {
   desc = "Open quickfix list on the right pane",
   noremap = true,
