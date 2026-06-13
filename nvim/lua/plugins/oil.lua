bcf697c9a33f8f83f31866ba59b914f816199a02
enhoshen <enhoshen@gmail.com>
2026-06-14 00:32:47 +0800

    nvim: Add suggested oil file detail toggle key map

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -3,6 +3,22 @@ return {
   opts = {
     keymaps = {
       ["?"] = { "actions.show_help", mode = "n" },
+      ["gd"] = {
+        desc = "Toggle file detail view",
+        callback = function()
+          detail = not detail
+          if detail then
+            require("oil").set_columns({
+              "icon",
+              "permissions",
+              "size",
+              "mtime",
+            })
+          else
+            require("oil").set_columns({ "icon" })
+          end
+        end,
+      },
     },
     view_options = {
       show_hidden = true,
