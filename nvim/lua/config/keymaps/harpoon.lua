1e7ce2467a81ae58678edf21183df408911549b9
enhoshen <enhoshen@gmail.com>
2026-07-26 11:42:32 +0800

    nvim: Add a-z to harpoon select keymap; Set save_on_toggle on for harpoon ui toggle

--- nvim/lua/config/keymaps/harpoon.lua
+++ nvim/lua/config/keymaps/harpoon.lua
@@ -3,6 +3,22 @@ local harpoon = require("harpoon")
 -- harpoon:setup()
 
 vim.keymap.set("n", "<leader>h", "", { desc = "Harpoon" })
+
+-- assign 1-9 and a-z to harpoon select
+local select_keys = {}
+for i = 1, 9 do
+  table.insert(select_keys, tostring(i))
+end
+for c = string.byte("a"), string.byte("z") do
+  table.insert(select_keys, string.char(c))
+end
+
+for idx, key in ipairs(select_keys) do
+  vim.keymap.set("n", "<leader>h" .. key, function()
+    harpoon:list():select(idx)
+  end, { desc = "Harpoon select item " .. idx })
+end
+
 vim.keymap.set("n", "<leader>ha", function()
   harpoon:list():add()
 end, { desc = "Harpoon add to list" })
@@ -13,15 +29,15 @@ vim.keymap.set("n", "<leader>hc", function()
   harpoon:list():clear()
 end, { desc = "Harpoon clear list" })
 vim.keymap.set("n", "<leader>hu", function()
-  harpoon.ui:toggle_quick_menu(harpoon:list())
+  harpoon.ui:toggle_quick_menu(harpoon:list(), {
+    save_on_toggle = true,
+    height_in_lines = math.min(
+      math.max(1, harpoon:list():length()),
+      vim.o.lines - 4
+    ),
+  })
 end, { desc = "Harpoon toggle list UI" })
 
--- assign 1-9 to harpoon select
-for i = 1, 9 do
-  vim.keymap.set("n", "<leader>h" .. i, function()
-    harpoon:list():select(i)
-  end, { desc = "Harpoon select " .. i .. "st item" })
-end
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<leader>hn", function()
   harpoon:list():prev()
