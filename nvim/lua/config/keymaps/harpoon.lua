1ae95b8b4ab9920d348a9eb5569af8decb763f77
enhoshen <enhoshen@gmail.com>
2026-07-26 12:17:58 +0800

    nvim: Add picker for harpoon
    by antigravity

--- nvim/lua/config/keymaps/harpoon.lua
+++ nvim/lua/config/keymaps/harpoon.lua
@@ -4,21 +4,6 @@ local harpoon = require("harpoon")
 
 vim.keymap.set("n", "<leader>h", "", { desc = "Harpoon" })
 
--- assign 1-9 and a-z to harpoon select
-local select_keys = {}
-for i = 1, 9 do
-  table.insert(select_keys, tostring(i))
-end
-for c = string.byte("a"), string.byte("z") do
-  table.insert(select_keys, string.char(c))
-end
-
-for idx, key in ipairs(select_keys) do
-  vim.keymap.set("n", "<leader>h" .. key, function()
-    harpoon:list():select(idx)
-  end, { desc = "Harpoon select item " .. idx })
-end
-
 vim.keymap.set("n", "<leader>ha", function()
   harpoon:list():add()
 end, { desc = "Harpoon add to list" })
@@ -45,3 +30,72 @@ end, { desc = "Harpoon next item" })
 vim.keymap.set("n", "<leader>hp", function()
   harpoon:list():next()
 end, { desc = "Harpoon previous item" })
+
+-- Snacks picker for Harpoon list
+local function harpoon_picker(opts)
+  opts = opts or {}
+  local harpoon_files = harpoon:list()
+  return require("snacks").picker(vim.tbl_deep_extend("force", {
+    title = "Harpoon",
+    finder = function()
+      local items = {}
+      for i = 1, harpoon_files:length() do
+        local item = harpoon_files.items[i]
+        if item and item.value and item.value ~= "" then
+          table.insert(items, {
+            file = item.value,
+            text = item.value,
+            pos = item.context
+                and { item.context.row or 1, item.context.col or 0 }
+              or nil,
+            harpoon_idx = i,
+          })
+        end
+      end
+      return items
+    end,
+    format = "file",
+    confirm = function(picker, item)
+      picker:close()
+      if item then
+        harpoon_files:select(item.harpoon_idx)
+      end
+    end,
+    actions = {
+      harpoon_delete = function(picker, item)
+        if item and item.harpoon_idx then
+          harpoon_files:remove_at(item.harpoon_idx)
+          picker:find()
+        end
+      end,
+    },
+    win = {
+      input = {
+        keys = {
+          ["<c-d>"] = { "harpoon_delete", mode = { "n", "i" } },
+        },
+      },
+    },
+  }, opts))
+end
+
+vim.keymap.set("n", "<leader>fH", function()
+  harpoon_picker()
+end, { desc = "Harpoon picker (Snacks)" })
+
+-- assign 1-9 and a-z to harpoon select if keymap is not already taken
+local select_keys = {}
+for i = 1, 9 do
+  table.insert(select_keys, tostring(i))
+end
+-- for c = string.byte("a"), string.byte("z") do
+--   table.insert(select_keys, string.char(c))
+-- end
+
+for idx, key in ipairs(select_keys) do
+  -- local lhs = "<leader>h" .. key
+  -- if vim.fn.maparg(lhs, "n") == "" then
+  vim.keymap.set("n", "<leader>h" .. key, function()
+    harpoon:list():select(idx)
+  end, { unique = true, desc = "Harpoon select item " .. idx })
+end
